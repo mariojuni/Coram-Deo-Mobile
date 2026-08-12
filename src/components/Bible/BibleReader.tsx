b0aba531a2bfeea0fa07f10905b1e4008a3d1040
Mario Juni <mjuni777@outlook.com>
2026-08-12 11:32:46 +0800

    style(bible): set line height to 30px

--- src/components/Bible/BibleReader.tsx
+++ src/components/Bible/BibleReader.tsx
@@ -601,7 +601,7 @@ const styles = StyleSheet.create({  container: { flex: 1, backgroundColor: '#faf
   },
   chapterContent: {
     fontSize: 18,
-    lineHeight: 34,
+    lineHeight: 30,
     fontFamily: 'Inter',
     color: '#1a1a1a',
   },
